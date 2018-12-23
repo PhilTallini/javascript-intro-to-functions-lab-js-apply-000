08e47359bdc4302b59b8173d5088213a3224deb2
Phil Tallini <philiptallini@gmail.com>
2018-12-23 22:09:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,12 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === 'I love you Grandma.') {
     return 'I love you, too';
-  } else if (string === string.toLowerCase()) {
-    return 'I can\'t hear you!'
-  } else if (string === string.toUpperCase() {
-    return 'YES UNDEED!'
+  } 
+  if (string === string.toLowerCase()) {
+    return 'I can\'t hear you!';
+  } 
+  if (string === string.toUpperCase() {
+    return 'YES UNDEED!';
   }
 }
 
