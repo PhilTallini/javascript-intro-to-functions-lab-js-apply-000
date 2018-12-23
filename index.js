eaa4e721437ee1de39d1b7b9c01d196dbedf13cc
Phil Tallini <philiptallini@gmail.com>
2018-12-23 22:10:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,5 @@ function sayHiToGrandma(string) {
   if (string === 'I love you Grandma.') {
     return 'I love you, too';
   } 
-  if (string === string.toLowerCase()) {
-    return 'I can\'t hear you!';
-  } 
 }
 
